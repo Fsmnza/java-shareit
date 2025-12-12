8dfbb3bc2d216ac98c00db6e1ba90afb76bc13f0
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-12-12 19:02:59 +0500

    исправление

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -8,29 +8,15 @@ import ru.practicum.shareit.booking.Status;
 
 import java.time.LocalDateTime;
 
-/**
- * DTO для создания или передачи информации о бронировании.
- */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookingDto {
-    /** Идентификатор бронирования */
     private Long id;
-
-    /** Идентификатор вещи, которую бронируют */
     private Long itemId;
-
-    /** Идентификатор пользователя, который бронирует */
     private Long bookerId;
-
-    /** Дата и время начала бронирования */
     private LocalDateTime start;
-
-    /** Дата и время окончания бронирования */
     private LocalDateTime end;
-
-    /** Статус бронирования */
     private Status status;
 }
